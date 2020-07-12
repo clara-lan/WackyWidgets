cdc766e557245819644d864b883c8b5168076190
Clara Lan <clarajylan@gmail.com>
2020-07-12 16:00:10 -0400

    feat: added input form and fixed css

--- main_app/models.py
+++ main_app/models.py
@@ -3,5 +3,3 @@ from django.db import models
 class Widget(models.Model):
   description = models.CharField(max_length=200)
   quantity = models.CharField(max_length=10)
-  def __str__(self):
-    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from .models import Widget
-
+from .forms import WidgetNewForm
+from django.views.generic.edit import CreateView, DeleteView
+from django.template import RequestContext
 def home(request):
   widgets = Widget.objects.all()
+  widget_form = WidgetNewForm()
   return render(request, 'index.html',{
-    'widgets':widgets
+    'widgets':widgets,
+    'widget_form':widget_form
   })
 
-
+class WidgetCreate(CreateView):
+  model = Widget
+  fields = '__all__'
+    # def form_valid(self, form):
+    # form.instance.user = self.request.user 
+    # return super().form_valid(form)
+class WidgetDelete(DeleteView):
+  model = Widget
+  success_url = ''
